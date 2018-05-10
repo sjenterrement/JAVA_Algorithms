ce413b07b87e19694ccaa9ee0b30a31225ec50cb
sjenterrement <sjenterrement@users.noreply.github.com>
2018-05-10 10:29:28 +0800

    modify

--- src/chapter_2/Pct_2_1_6.java
+++ src/chapter_2/Pct_2_1_6.java
@@ -16,15 +16,16 @@ public class Pct_2_1_6 {
 		for (int i = 0; i < N; i++) {
 			a[i]=32;//设置任意相同主键数组
 		}
+		Stopwatch timer2=new Stopwatch();
+		Insertion.sort(a);
+		t2=timer2.elapsedTime();
+		StdOut.println("insertion time: "+t2+"s");
 		Stopwatch timer1=new Stopwatch();
 		Selection.sort(a);
 		t1=timer1.elapsedTime();
 		StdOut.println("selection time: "+t1+"s");
 
-		Stopwatch timer2=new Stopwatch();
-		Insertion.sort(a);
-		t2=timer2.elapsedTime();
-		StdOut.println("insertion time: "+t2+"s");
+		
 		
 		if(t1>t2)
 			StdOut.println("insertion fast");
--- src/chapter_2/Pct_2_1_7.java
+++ src/chapter_2/Pct_2_1_7.java
@@ -2,49 +2,43 @@ package chapter_2;
 
 import edu.princeton.cs.algs4.Insertion;
 import edu.princeton.cs.algs4.Selection;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class Pct_2_1_7 {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int N=2000,T=100;
-		double t1=0,t2=0;
-		Integer[] a=new Integer[N];
+		String alg1 = StdIn.readString();
+		String alg2 = StdIn.readString();
+		int N = StdIn.readInt();
+		int T = StdIn.readInt();
+		double t1 = timeInverseInput(alg1, N, T);
+		double t2 = timeInverseInput(alg2, N, T);
+		StdOut.printf("for %d inverse Doules\n  %s is", N, alg1);
+		StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+
+	}
+
+	public static double timeInverseInput(String alg, int N, int T) {
+		double total = 0.0;
+		Double[] a = new Double[N];
 		for (int t = 0; t < T; t++) {
 			for (int i = 0; i < N; i++) {
-			a[i]=1*(N-i);
+				a[i] = (1.0 * (N - i));
 			}
-			t1+=time1(a);
-			t2+=time2(a);
+			total += time(alg, a);
 		}
-		
-		StdOut.println("selection time: "+t1+"s");
-
-		StdOut.println("Insertion time: "+t2+"s");
-		
-		if(t1>t2)
-			StdOut.println("insertion fast");
-		else
-			StdOut.println("selection fast");
-		
+		return total;
 	}
-	public static double time1(Integer[] a) {
-	
-		Stopwatch timer1=new Stopwatch();
-		Selection.sort(a);
-		return timer1.elapsedTime();
-		
-		}
-		
-	
-	public static double time2(Integer[] a) {
-		
-		Stopwatch timer2=new Stopwatch();
-		Insertion.sort(a);
-		return timer2.elapsedTime();
-		
-	
+
+	public static double time(String alg, Double[] a) {
+		Stopwatch timer = new Stopwatch();
+		if (alg.equals("Insertion"))
+			Insertion.sort(a);
+		if (alg.equals("Selection"))
+			Selection.sort(a);
+		return timer.elapsedTime();
 	}
 }
