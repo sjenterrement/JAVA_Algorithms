186fb0c1f97a0f37730ed72de6f685218ed2e3e5
sjenterrement <sjenterrement@users.noreply.github.com>
2018-05-27 23:25:32 +0800

    2018.5.27...

---
+++ src/leetCode/RomanToInt.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import java.util.Scanner;
+
+public class RomanToInt {
+	
+	 public static void romanToInt(String s) {
+		 String a="01";
+		 s.substring();
+	        System.out.println(s);
+	        
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input =new Scanner(System.in);
+		String s=input.next();
+		romanToInt(s);
+
+	}
+
+}
