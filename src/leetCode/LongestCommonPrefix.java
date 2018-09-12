59c3f1151485ecf87a3c040019b03fc790755faa
sjenterrement <31695808+sjenterrement@users.noreply.github.com>
2018-09-12 19:55:28 +0800

    modify//9.12

--- src/leetCode/LongestCommonPrefix.java
+++ src/leetCode/LongestCommonPrefix.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 
 public class LongestCommonPrefix {
 
-	public void LongestCommonPrefix(String[] strs) {
-		String[] s= {"leets","leetcode","leet","lee"};
-		if(s.length==0)
-			System.out.println("error");
-		
-		String prefix=s[0];
-		
+	public static void main(String[] args) {
+
+		String[] s = { "leets", "leetcode", "leet", "lee" };
+		if (s.length == 0)
+			System.out.println("string is empty");
+
+		String prefix = s[0];
+
 		for (int i = 1; i < s.length; i++) {
-			while (s[i].indexOf(prefix)!=0) {
-				prefix=prefix.substring(0, prefix.length()-1);
+			while (s[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length() - 1);
 				if (prefix.isEmpty()) {
-					System.out.println("没有相同的前缀");
+					System.out.println("no prefix");
 				}
 			}
 		}
+		
+		System.out.println("the prefix is "+prefix);
 	}
 }
