5a20768dd951da2f0624023e086c8a9b0e2167c3
sjenterrement <sjenterrement@gmail.com>
2018-09-13 22:47:28 +0800

    添加注释，9.13

--- src/leetCode/LongestCommonPrefix.java
+++ src/leetCode/LongestCommonPrefix.java
@@ -13,7 +13,7 @@ public class LongestCommonPrefix {
 		String prefix = s[0];
 
 		for (int i = 1; i < s.length; i++) {
-			while (s[i].indexOf(prefix) != 0) {
+			while (s[i].indexOf(prefix) != 0) {//注意！！这是判断prefix是否是s[i]的前缀，s[i].indexOf(prefix) != -1才是判断是否是prefix包含在s[i]中
 				prefix = prefix.substring(0, prefix.length() - 1);
 				if (prefix.isEmpty()) {
 					System.out.println("no prefix");
